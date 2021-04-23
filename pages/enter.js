7759e057a95089a777cca29a05871eb8ea54d03f
jay <jay@jay.localdomain>
2021-04-23 15:21:34 +0700

    google signin popup

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,38 @@
+import { auth, googleAuthProvider } from "@lib/firebase";
+
 export default function EnterPage() {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h1>Sign up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={`/google.png`} width="30px" /> Sign in with Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>SignOut</button>;
+}
+
+function UsernameForm() {}
