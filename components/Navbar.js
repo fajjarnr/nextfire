ddcab055cd5fb9620d2298e37f5b08a09f5b9f22
jay <jay@jay.localdomain>
2021-04-25 06:41:42 +0700

    user form

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,45 @@
 import { UserContext } from "@lib/context";
 import Link from "next/link";
 import { useContext } from "react";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { user, username } = useContext(UserContext);
+
+  const router = useRouter();
+
+  const signOut = () => {
+    auth.signOut();
+    router.reload();
+  };
 
   return (
     <nav className="navbar">
       <ul>
         <li>
           <Link href="/">
-            <button className="btn-logo">FEED</button>
+            <button className="btn-logo">NEXT</button>
           </Link>
         </li>
+
         {username && (
           <>
             <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+            <li>
               <Link href="/admin">
-                <button className="btn-blue">Write Post</button>
+                <button className="btn-blue">Write Posts</button>
               </Link>
             </li>
             <li>
               <Link href={`/${username}`}>
-                <img src={user?.photoUrl} />
+                <img src={user?.photoURL || "/hacker.png"} />
               </Link>
             </li>
           </>
         )}
+
         {!username && (
           <li>
             <Link href="/enter">
